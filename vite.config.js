400ca2f0a527c16bf036cc65f776f13f821a0cff
nataliarmorais <natalya.rios@gmail.com>
2024-12-03 21:53:54 -0300

    Deploy inicial para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Doacoes-de-Livros-Vai-na-Web/', // Substitua <nome-do-repositorio> pelo nome do seu repositório no GitHub
 })
